de891f202af07db1b34ca458ab721f34a05c927a
fikreab123 <fmezgebu@terpmail.umd.edu>
2025-05-11 22:08:01 -0400

    Revert "Merge branch 'main' of https://github.com/Bereket-lab/Final_Project"
    This reverts commit a47dbb99b1f07f3d75ea6598f0008a67aa1fdc99, reversing
    changes made to 53f63527e944dc7e7fc8fbec6e57f2b08546e291.

--- user_input.py
+++
@@ -1,7 +0,0 @@
-def get_user_input():
-    weight = float(input("Enter your weight (kg): "))
-    height = float(input("Enter your height (cm): "))
-    age = int(input("Enter your age: "))
-    sex = input("Enter your sex (male/female): ").strip().lower()
-    activity = input("Enter your activity level (low/medium/high): ").strip().lower()
-    return weight, height, age, sex, activity
